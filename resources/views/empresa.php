4e699cbba5237479a5bd5ba678b7d917b90baf64
lucascarvalho6 <lucas.carvalho269@etec.sp.gov.br>
2023-02-24 17:35:45 -0300

    Criação de rotas - 24/02

---
+++ resources/views/empresa.php
@@ -0,0 +1,5 @@
+<h1>Empresa</h1>
+
+<a href="/">Home</a>
+<a href="produtos">Produtos</a>
+<a href="contato">Contato</a>
---
+++ resources/views/produtos.php
@@ -0,0 +1,5 @@
+<h1>Produtos</h1>
+
+<a href="/">Home</a>
+<a href="empresa">Empresa</a>
+<a href="contato">Contato</a>
